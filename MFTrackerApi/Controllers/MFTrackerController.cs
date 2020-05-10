7aaf284f4fc268f41ae20ef01806abb47b6d300d
Vaibhav <vaibhavd.consistent@gmail.com>
2020-05-10 08:27:00 +0530

    Add project files.

---
+++ MFTrackerApi/Controllers/MFTrackerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MFTrackerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MFTrackerController : ControllerBase
+    {
+        // GET: api/MFTracker
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/MFTracker/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        //// POST: api/MFTracker
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT: api/MFTracker/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
